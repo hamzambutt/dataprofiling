22feffe4971e2433cd3a72b662856d96dd27c090
Hamza <hamzamasood420@gmail.com>
2026-04-09 18:14:26 +0500

    fixed repo structure

---
+++ dont need/docs/api.md
@@ -0,0 +1,15 @@
+# Data generation
+::: driftbench.data_generation.loaders
+::: driftbench.data_generation.sample
+::: driftbench.data_generation.drifts
+::: driftbench.data_generation.solvers
+::: driftbench.data_generation.latent_information
+
+# Drift detection
+
+::: driftbench.drift_detection.detectors
+::: driftbench.drift_detection.metrics
+::: driftbench.drift_detection.helpers
+
+# Benchmarks
+::: driftbench.benchmarks.data
---
+++ src/dynamic.py
@@ -0,0 +1,60 @@
+import numpy as np
+import pandas as pd
+import os
+
+
+class Profiling:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.df = self.load_data()
+
+    def load_data(self):
+        ext = os.path.splitext(self.file_path)[1].lower()
+
+        if ext == '.csv':
+            return pd.read_csv(self.file_path)
+        elif ext == '.npy':
+            data = np.load(self.file_path)
+            return pd.DataFrame(
+                data, columns=[
+                    f"feature_{i}" for i in range(
+                        data.shape[1])])
+        elif ext == '.parquet':
+            return pd.read_parquet(self.file_path)
+        else:
+            raise ValueError(f"Unsupported file format: {ext}")
+
+    def data_stats(self, p_list=[]):
+
+        unique_values = np.unique(self.df)
+
+        stats = {
+            "metadata": {
+                "total_rows": self.df.shape[0],
+                "total_columns": self.df.shape[1],
+
+                "column_types": self.df.dtypes.astype(str).to_dict(),
+
+                "unique_count": len(unique_values),
+                "null_percentage": (np.isnan(self.df).mean() * 100)
+            },
+
+            "summaray": {
+
+                "mean": np.mean(self.df),
+                "median": np.median(self.df),
+                "min": np.min(self.df),
+                "max": np.max(self.df)
+            }
+        }
+
+        p_values = np.percentile(self.df, p_list)
+
+        for p, val in zip(p_list, p_values):
+            stats[f"percentile_{p}"] = val
+        return stats
+
+
+profiler = Profiling('dataset_2.npy')
+percentt = [25, 50, 75]
+print(profiler.data_stats(percentt))
